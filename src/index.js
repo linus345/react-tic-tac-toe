28a6e57070520974bd72278cf264d651c178e46f
linus345 <linusekman01@gmail.com>
2019-01-20 20:43:53 +0100

    Restructuring components

--- src/index.js
+++ src/index.js
@@ -1,58 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-class Square extends Component {
-  render() {
-    return(
-      <button className="square">
-
-      </button>
-    )
-  }
-}
-
-class Board extends Component {
-  renderSquare(i) {
-    return <Square />;
-  }
-
-  render() {
-    const status = 'Next player: X';
-    return(
-      <div>
-        <div className="status">{ status }</div>
-        <div className="board-row">
-          { this.renderSquare(0) }
-          { this.renderSquare(1) }
-          { this.renderSquare(2) }
-        </div>
-        <div className="board-row">
-          { this.renderSquare(3) }
-          { this.renderSquare(4) }
-          { this.renderSquare(5) }
-        </div>
-        <div className="board-row">
-          { this.renderSquare(6) }
-          { this.renderSquare(7) }
-          { this.renderSquare(8) }
-        </div>
-      </div>
-    )
-  }
-}
-
-class Game extends Component {
-  render() {
-    return(
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info"></div>
-      </div>
-    )
-  }
-}
+import Game from './components/Game';
 
 ReactDOM.render(<Game />, document.getElementById('root'));
